1c42353031fce3f0b0769baa0bf363f3b4ef8fbd
Diego Avila <42280085+DiegoAvmor@users.noreply.github.com>
2020-03-14 19:01:30 -0600

    Add authentication module php

---
+++ php/authenticate_user.php
@@ -0,0 +1,39 @@
+<?php
+include_once 'db.php';
+include 'model/SimpleResponse.php';
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+$response = authenticate($email,$password);
+echo $response -> get_JSON();
+
+function authenticate($email,$password){
+    $dbconnection= establishConnectionDB();
+    $response = new SimpleResponse(403,"Invalid Administrator");
+    if($dbconnection->has("administrador",["email"=> $email])){
+        //Es un administrador, hace falta validar que su contraseña
+        $credentials = $dbconnection->get("administrador",[
+            "[<]usuarios"=>"email"
+        ],[
+            "administrador.email",
+            "usuarios.password"
+        ],[
+            "administrador.email" => $email
+        ]);
+        if($credentials['password']===$password){
+            $response -> set_status(200);
+            $response -> set_message("../MyScheduleLite/schedule.html");
+            session_start();
+            $_SESSION['access'] = true;
+            $_SESSION['user'] = $email;
+        }else{
+            $response -> set_message("Invalid Credentials, Try Again");
+        }
+    }
+    return $response;
+}
+
+
+
+?>
