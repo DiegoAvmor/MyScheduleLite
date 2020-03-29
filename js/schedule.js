2c8c1e8419904a496c8a985abe9067aae1cd3a32
Diego Avila <42280085+DiegoAvmor@users.noreply.github.com>
2020-03-29 16:29:12 -0600

    Add schedule loader

--- js/schedule.js
+++ js/schedule.js
@@ -1,3 +1,27 @@
 $(document).ready(function(){
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const key_value = urlParams.get('clave_grupo')
+    getSubjectSchedules(key_value);
+    console.log(key_value);
     console.log(Cookies.get('name')+ "hola");
 });
+
+
+
+function getSubjectSchedules(clave_grupo){
+    $.ajax({
+        type: "GET",
+        url: "../php/manage_subject_data.php",
+        data: {
+            "clave_grupo": clave_grupo
+        }
+      })
+        .done(handleResponse)
+        .fail((xhr, status, error) => console.log(error));
+}
+
+const handleResponse = response =>{
+    console.log(response);
+    console.log(JSON.parse(response));
+}
