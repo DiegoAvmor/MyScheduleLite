2e040bba45221fec5dfbc36aea61e45b09c18626
Diego Avila <42280085+DiegoAvmor@users.noreply.github.com>
2020-03-30 15:30:22 -0600

    Modify deconstruction of offer response

--- js/schedule.js
+++ js/schedule.js
@@ -37,5 +37,38 @@ const handleResponse = response =>{
 
 const handleOffer = response =>{
     let parsedResponse = JSON.parse(response);
-    console.log(parsedResponse);
+    deconstructSubjectResponse(parsedResponse.maestro_materia);
+}
+
+let subjects_offer = new Array(); //Arreglo de materias
+let map_subjects_teachers; //Mapa de materias respecto a maestros
+
+function deconstructSubjectResponse(subject_teacher_relations){
+    subject_teacher_relations.forEach(relation => {
+        if(!checkSubjectInArray(relation.clave_materia)){
+            subjects_offer.push({"clave_materia":relation.clave_materia,"nombre_materia":relation.nombre_materia});
+        }
+    });
+    map_subjects_teachers = subjects_offer.map(subject => {
+        let relation = {};
+        relation["key"] = subject.clave_materia;
+        relation["teachers"] = getTeachersBySubjectId(subject.clave_materia,subject_teacher_relations);
+        return relation;
+    });
+    console.log(subjects_offer);
+    console.log(map_subjects_teachers);
+}
+
+function checkSubjectInArray(subject_id){
+    return subjects_offer.find(subject => subject.clave_materia == subject_id);
+}
+
+function getTeachersBySubjectId(subject_id, array){
+    let teachers = new Array();
+    array.forEach(relation => {
+        if(relation.clave_materia == subject_id){
+            teachers.push({"clave_maestro": relation.clave_maestro , "nombre_maestro": relation.nombre_maestro});
+        }
+    });
+    return teachers;
 }
