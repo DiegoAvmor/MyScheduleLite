376b21edc5c87b21933fa23fc598050e986e9baf
Manuel Martin Rico <manuelrico98@outlook.es>
2020-04-04 15:47:11 -0600

    pop up de grids
    Se agrega  el pop up para editar o eliminar horarios
    
    *falta consultar con equipo de trabajo

--- js/schedule/schedule.js
+++ js/schedule/schedule.js
@@ -115,45 +115,6 @@ const handleResponse = response =>{
     }
 }
 
-/**
- * 
-<div class = "scheduleinfo">
-    <div class = "scheduletitle montserratfont font16 fontsemibold">Fundamentos de ingnieria de software</div><img src = "../images/iconos/options.png" class = "optionsbuton">
-    <img id = "horizontalseparator" src = "../images/iconos/horizontalseparator.png">
-    <p class = "montserratfont font16">Nombre del maestro</p>
-    <p class = "montserratfont font16">Aula</p>
-</div>
- */
-function chargeSubjectsTable(subject){
-    let newschedulelement = document.createElement('div');
-    newschedulelement.setAttribute("class","scheduleinfo");
-    let newsubjecttitle = document.createElement('div');
-    newsubjecttitle.setAttribute("class","scheduletitle montserratfont font16 fontsemibold");
-    newsubjecttitle.innerHTML = subject.nombre_materia;
-    let newoptionsbutton = document.createElement("img");
-    newoptionsbutton.setAttribute("src","../images/iconos/options.png");
-    newoptionsbutton.setAttribute("class","optionsbuton");
-    let newseparator = document.createElement("img");
-    newseparator.setAttribute("id","horizontalseparator");
-    newseparator.setAttribute("src","../images/iconos/horizontalseparator.png");
-    let newteachername = document.createElement("p");
-    newteachername.setAttribute("class","montserratfont font16");
-    newteachername.innerHTML = subject.nombre_maestro;
-    let newroomname = document.createElement("p");
-    newroomname.setAttribute("class","montserratfont font16");
-    newseparator.innerHTML = "Aula: " + subject.clave_aula;
-    newschedulelement.append(newsubjecttitle);
-    newschedulelement.append(newoptionsbutton);
-    newschedulelement.append(newseparator);
-    newschedulelement.append(newteachername);
-    newschedulelement.append(newroomname);
-    console.log(subject.dia_semana);
-    console.log(subject.hora_inicio);
-    console.log(weekdays[subject.dia_semana]);
-    console.log(timesmorning[subject.hora_inicio]);
-   document.getElementById("scheduletable").rows[timesmorning[subject.hora_inicio]].cells[weekdays[subject.dia_semana]].append(newschedulelement);
-}
-
 const handleOffer = response =>{
     let parsedResponse = JSON.parse(response);
     deconstructSubjectResponse(parsedResponse.maestro_materia);
@@ -191,6 +152,61 @@ function getTeachersBySubjectId(subject_id, array){
     return teachers;
 }
 
+/**
+ * 
+<div class = "scheduleinfo">
+    <div class = "scheduletitle montserratfont font16 fontsemibold">Fundamentos de ingnieria de software</div><img src = "../images/iconos/options.png" class = "optionsbuton">
+    <img id = "horizontalseparator" src = "../images/iconos/horizontalseparator.png">
+    <p class = "montserratfont font16">Nombre del maestro</p>
+    <p class = "montserratfont font16">Aula</p>
+</div>
+ */
+function chargeSubjectsTable(subject){
+    let newschedulelement = document.createElement('div');
+    newschedulelement.setAttribute("class","scheduleinfo");
+    let newsubjecttitle = document.createElement('div');
+    newsubjecttitle.setAttribute("class","scheduletitle montserratfont font16 fontsemibold");
+    newsubjecttitle.innerHTML = subject.nombre_materia;
+    let newoptionsbutton = document.createElement("button");
+    newoptionsbutton.setAttribute("onclick","openModalButtons()");
+    newoptionsbutton.setAttribute("class","optionsbuton");
+    let newseparator = document.createElement("img");
+    newseparator.setAttribute("id","horizontalseparator");
+    newseparator.setAttribute("src","../images/iconos/horizontalseparator.png");
+    let newteachername = document.createElement("p");
+    newteachername.setAttribute("class","montserratfont font16");
+    newteachername.innerHTML = subject.nombre_maestro;
+    let newroomname = document.createElement("p");
+    newroomname.setAttribute("class","montserratfont font16");
+    newroomname.innerHTML = "Aula: " + subject.clave_aula;
+    newschedulelement.append(newsubjecttitle);
+    newschedulelement.append(newoptionsbutton);
+    newschedulelement.append(newseparator);
+    newschedulelement.append(newteachername);
+    newschedulelement.append(newroomname);
+    console.log(subject.dia_semana);
+    console.log(subject.hora_inicio);
+    console.log(weekdays[subject.dia_semana]);
+    console.log(timesmorning[subject.hora_inicio]);
+    document.getElementById("scheduletable").rows[timesmorning[subject.hora_inicio]].cells[weekdays[subject.dia_semana]].append(newschedulelement);
+}
+
 function displayWindowSize(){
     document.getElementById("barratareas").style.height = document.documentElement.clientHeight - 110 + "px";
+}
+
+window.addEventListener("resize",displayWindowSize);
+
+function openModalButtons(){
+    const modal = document.getElementById('optionspopup');
+    if(modal == null) return;
+    modal.classList.add('active');
+    overlay.classList.add('active');
+}
+
+function closeModalButtons(){
+    const modal = document.getElementById('optionspopup');
+    if(modal == null) return;
+    modal.classList.remove('active');
+    overlay.classList.remove('active');
 }
