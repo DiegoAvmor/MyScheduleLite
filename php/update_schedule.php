e349ad29f7d2b5d188de4aea0aa23bafa572dbec
JohnCaceres <43764825+JohnCaceres@users.noreply.github.com>
2020-05-11 19:06:12 -0500

    Update PHP added
    -Checks if the same teacher isn't in another classroom at the same hour.
    -Adds the scheduled class to the DB

--- php/update_schedule.php
+++ php/update_schedule.php
@@ -1,23 +1,50 @@
 <?php
 include_once 'db.php';
 include 'model/SimpleResponse.php';
+if(isset($_POST["horario"])&&isset($_POST["clave"])){
+    $inputHor= json_decode($_POST['horario']);
+    $inputClav=json_decode($_POST['clave']);
+    $callB = update($inputHor,$inputClav);
+    $resp=$callB->get_JSON();
+    echo $resp;
+}else{
+    echo "suck me nuts";
+}
 
-$response = update();
-echo $response -> get_JSON();
+function update($horario,$claveGrupo){
 
-function update(){
     $dbconnection= establishConnectionDB();
     $response = new SimpleResponse(404,"Resource not founded");
-    //Falta añadir sentencia SQL
-    if($name){
-        $response -> set_status(200);
-        $response -> set_message("Llegamos al update, Sir");
+   $count = $dbconnection->count("horario", [
+        "clave_maestro" => $horario->clave_maestro,
+        "hora_inicio"=> $horario->hora_inicio,
+        "hora_termina"=> $horario->hora_termina,
+        "dia_semana"=> $horario->dia_semana
+        ]);    
+    if(!$count){
+        echo $count;
+        if ($count<1){
+            $check=$dbconnection->insert("horario", [
+                "clave_aula" => $horario->clave_aula,
+                "clave_maestro" => $horario->clave_maestro,
+                "clave_materia" => $horario->clave_materia,
+                "clave_grupo" =>$claveGrupo,
+                "hora_inicio" => $horario->hora_inicio,
+                "hora_termina" => $horario->hora_termina,
+                "dia_semana" => $horario->dia_semana
+            ]);
+
+            if($check->rowCount()>0){
+                $response -> set_status(200);
+                $response -> set_message("Llegamos al update, Sir");
+            }else{
+                $response -> set_message("Hay problemas en el update, Sir");
+            }
+        }
     }else{
         $response -> set_message("Hay problemas en el update, Sir");
     }
-    
     return $response;
 }
-
 
 ?>
