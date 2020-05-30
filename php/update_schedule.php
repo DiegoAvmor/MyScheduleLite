98c0118d53ca75402527f77b486351ea772caccd
JohnCaceres <43764825+JohnCaceres@users.noreply.github.com>
2020-05-30 12:51:45 -0500

    Update update_schedule.php
    -Deleted old validation

--- php/update_schedule.php
+++ php/update_schedule.php
@@ -14,36 +14,26 @@ if(isset($_POST["horario"])&&isset($_POST["clave"])){
 function update($horario,$claveGrupo){
 
     $dbconnection= establishConnectionDB();
-    $response = new SimpleResponse(404,"Resource not founded");
-   $count = $dbconnection->count("horario", [
-        "clave_maestro" => $horario->clave_maestro,
-        "hora_inicio"=> $horario->hora_inicio,
-        "hora_termina"=> $horario->hora_termina,
-        "dia_semana"=> $horario->dia_semana
-        ]);    
-    if(!$count){
-        echo $count;
-        if ($count<1){
-            $check=$dbconnection->insert("horario", [
-                "clave_aula" => $horario->clave_aula,
-                "clave_maestro" => $horario->clave_maestro,
-                "clave_materia" => $horario->clave_materia,
-                "clave_grupo" =>$claveGrupo,
-                "hora_inicio" => $horario->hora_inicio,
-                "hora_termina" => $horario->hora_termina,
-                "dia_semana" => $horario->dia_semana
+    $response = new SimpleResponse(404,"Resource not found");
+ 
+        $check=$dbconnection->insert("horario", [
+            "clave_aula" => $horario->clave_aula,
+            "clave_maestro" => $horario->clave_maestro,
+            "clave_materia" => $horario->clave_materia,
+            "clave_grupo" =>$claveGrupo,
+            "hora_inicio" => $horario->hora_inicio,
+            "hora_termina" => $horario->hora_termina,
+            "dia_semana" => $horario->dia_semana
             ]);
 
-            if($check->rowCount()>0){
-                $response -> set_status(200);
-                $response -> set_message("Horario añadido con éxito.");
-            }else{
-                $response -> set_message("El horario no pudo ser añadido a la BD.");
-            }
+        if($check->rowCount()>0){
+            $response -> set_status(200);
+            $response -> set_message("Horario añadido con éxito.");
+        }else{
+            $response -> set_message("El horario no pudo ser añadido a la BD.");
         }
-    }else{
-        $response -> set_message("No se pudo añadir el horario: el mismo maestro tiene coincidencias en la misma hora y día de la semana.");
-    }
+        
+    
     return $response;
 }
 
